c04453585c6575fe2beb557ab192560fab5b610a
Ian <iumpoy@yahoo.com>
2021-08-11 22:24:43 -0700

    converted list into a table

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -4,24 +4,34 @@ export default ({ stocks, updateQuantity }) => {
 
     const mappedStocks = stocks.map(item => {
         return (
-            <div className="stock d-flex justify-content-between" key={item.symbol}>
-                <div className="stock-ticker">{item.symbol}</div>
-                <div className="stock-price">{item.price}</div>
+            <tr key={item.symbol}>
+                <td className="stock-ticker">{item.symbol}</td>
+                <td className="stock-price">{item.price}</td>
                 <input
                     type="number"
                     onChange={(e) => { updateQuantity(e.target.value, item.symbol) }}
                     value={item.quantity}
                 />
-                <div className="stock-equaity">{item.price * item.quantity}</div>
-                <div className="stock-dividend">{item.dividend}%</div>
-                <div className="stock-passive-income">{(item.price * (item.dividend / 100)).toFixed(2) * item.quantity}</div>
+                <td className="stock-equaity">{item.price * item.quantity}</td>
+                <td className="stock-dividend">{item.dividend}%</td>
+                <td className="stock-passive-income">{(item.price * (item.dividend / 100)).toFixed(2) * item.quantity}</td>
 
-            </div>
+            </tr>
         )
     })
     return (
         <div className="stock-list">
-            {mappedStocks}
+            <table style={{ width: '100%' }}>
+                <tr>
+                    <th>Stock</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Equaity</th>
+                    <th>Dividend</th>
+                    <th>Passive Income</th>
+                </tr>
+                {mappedStocks}
+            </table>
         </div>
     )
 }
