e3b916df715cf940f83ecfdaa2e8da48be4e66dc
umpoy <iumpoy@yahoo.com>
2022-09-09 22:12:15 -0700

    feat added json to store stock list

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,12 +1,19 @@
 const getStock = require("../functions/getStockData.js");
+const fs = require("fs");
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send("Welcome to my stock API");
-    });
+  app.get("/", (req, res) => {
+    res.send("Welcome to my stock API");
+  });
 
-    app.get('/getStock/:tickerSymbol', async function (req, res) {
-        const ticker = req.params.tickerSymbol;
-        res.send(await getStock(ticker));
-    });
-}
+  app.get("/getStock/:tickerSymbol", async (req, res) => {
+    const ticker = req.params.tickerSymbol;
+    res.send(await getStock(ticker));
+  });
+
+  app.get("/getMyStockData", (req, res) => {
+    let rawdata = fs.readFileSync(__dirname + "/data.json");
+    let stocks = JSON.parse(rawdata);
+    console.log(stocks);
+  });
+};
