5d641f3ba5beec909c1b1c23a08c5640613b2cbc
umpoy <iumpoy@yahoo.com>
2024-11-01 18:01:55 -0700

    feat: update webscrapped site

--- server/functions/getStockData.js
+++ server/functions/getStockData.js
@@ -1,17 +1,30 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
 module.exports = async function (symbol) {
-    const res = await fetch(`https://www.marketwatch.com/investing/stock/${symbol}`);
-    const text = await res.text();
-    const $ = cheerio.load(text);
-    const findStockPrice = $('.intraday__price .value').text();
-    const findDividend = $('.list--kv li small:contains("Yield")').next('.primary').text().slice(0, -1);
+  const res = await fetch(
+    `https://www.google.com/finance/quote/${symbol}:NASDAQ?hl=en`
+  );
+  const text = await res.text();
+  const $ = cheerio.load(text);
+  const findStockPrice = $(".YMlKec.fxKbKc")
+    .first()
+    .text()
+    .replace(/[$,]/g, "")
+    .trim();
+  const findDividend = $(".gyFHrc")
+    .filter((i, el) => {
+      return $(el).find(".mfs7Fc").text().trim() === "Dividend yield";
+    })
+    .find(".P6K39c")
+    .text()
+    .replace(/[%]/g, "")
+    .trim();
 
-    const data = {
-        symbol: symbol,
-        price: Number(findStockPrice),
-        dividend: Number(findDividend)
-    }
-    return await data
+  const data = {
+    symbol: symbol,
+    price: Number(findStockPrice),
+    dividend: Number(findDividend),
+  };
+  return await data;
 };
