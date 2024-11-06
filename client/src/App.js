884805146e5fd059cbf961584b7b3666efc765ea
umpoy <iumpoy@yahoo.com>
2024-11-06 11:26:31 -0800

    feat: added delete button to update state

--- client/src/App.js
+++ client/src/App.js
@@ -1,89 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import StockList from './components/List';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import StockList from "./components/List";
+import axios from "axios";
 
 const App = () => {
-    const [ticker, setTicker] = useState('');
-    const [stocks, setStock] = useState([]);
-    const [networth, setNetworth] = useState(0);
-    const [passiveIncome, setPassiveIncome] = useState(0);
-    const [result, setResults] = useState({});
-    useEffect(() => {
-        const searchStock = async () => {
-            const { data } = await axios.get(`http://localhost:5000/getStock/${ticker}`);
-            setResults(data);
-        }
-        const debounce = setTimeout(() => {
-            if (ticker) {
-                searchStock();
-            }
-        }, 500);
-        return () => {
-            clearTimeout(debounce);
-        }
-    }, [ticker]);
-    useEffect(() => {
-        let total = 0;
-        let payout = 0;
+  const [ticker, setTicker] = useState("");
+  const [stocks, setStock] = useState([]);
+  const [networth, setNetworth] = useState(0);
+  const [passiveIncome, setPassiveIncome] = useState(0);
+  const [result, setResults] = useState({});
 
-        stocks.forEach(item => {
-            total += item.quantity * item.price;
-            payout += (item.price * (item.dividend / 100)).toFixed(2) * item.quantity
-        })
-        setNetworth(total);
-        setPassiveIncome(payout);
-    }, [stocks])
-
-    const handleOnSubmit = e => {
-        e.preventDefault();
-        result.quantity = 1
-        setStock(stocks.concat(result))
-        setTicker('');
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!result.price) return;
+    result.quantity = 1;
+    setStock([result, ...stocks]);
+    setTicker("");
+  };
+  const showStockInfo = () => {
+    if (ticker.length === 0) {
+      return <p>Search stock by ticker symbol</p>;
     }
-    const showStockInfo = () => {
-        if (ticker.length === 0) {
-            return (
-                <p>Search stock by ticker symbol</p>
-            )
-        }
-        if (result.price > 0 && ticker) {
-            return (
-                <p>{result.symbol} is at {result.price} with a {result.dividend}% dividend</p>
-            )
-        } else {
-            return (
-                <p>Sorry that stock either doesn't exist or doesn't have a dividend</p>
-            )
-        }
+    if (result.price > 0 && ticker) {
+      return (
+        <p>
+          {result.symbol} is at {result.price} with a {result.dividend}%
+          dividend
+        </p>
+      );
+    } else {
+      return (
+        <p>Sorry that stock either doesn't exist or doesn't have a dividend</p>
+      );
     }
+  };
 
-    const updateStockQuantity = (e, stockTicker) => {
-        const foundStock = stocks.find(({ symbol }) => symbol === stockTicker);
-        foundStock.quantity = e;
-        const index = stocks.findIndex(({ symbol }) => symbol === stockTicker);
-        const newState = [...stocks];
-        newState[index] = foundStock;
-        setStock(newState);
-    }
+  const updateStockQuantity = (e, stockTicker) => {
+    const foundStock = stocks.find(({ symbol }) => symbol === stockTicker);
+    foundStock.quantity = e;
+    const index = stocks.findIndex(({ symbol }) => symbol === stockTicker);
+    const newState = [...stocks];
+    newState[index] = foundStock;
+    setStock(newState);
+  };
 
-    return (
-        <div div className="app">
-            <div className="ticker-search">
-                <form onSubmit={handleOnSubmit}>
-                    <input
-                        type="text"
-                        value={ticker}
-                        onChange={e => { setTicker(e.target.value) }}
-                    />
-                </form>
-                {showStockInfo()}
-            </div>
-            <div className="assets">
-                <h2>Networth: {networth}</h2>
-                <h2>Yearly dividend: {passiveIncome}</h2>
-            </div>
-            <StockList stocks={stocks} updateQuantity={updateStockQuantity} />
-        </div>
-    )
-}
+  const deleteStock = (stockTicker) => {
+    console.log(stocks);
+    const updatedList = stocks.filter((stock) => stock.symbol !== stockTicker);
+    setStock(updatedList);
+  };
+
+  useEffect(() => {
+    const searchStock = async () => {
+      const { data } = await axios.get(
+        `http://localhost:5000/getStock/${ticker}`
+      );
+      setResults(data);
+    };
+    const debounce = setTimeout(() => {
+      if (ticker) {
+        searchStock();
+      }
+    }, 1000);
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [ticker]);
+
+  useEffect(() => {
+    let total = 0;
+    let payout = 0;
+
+    stocks.forEach((item) => {
+      total += item.quantity * item.price;
+      payout += (item.price * (item.dividend / 100)).toFixed(2) * item.quantity;
+    });
+    setNetworth(total);
+    setPassiveIncome(payout);
+  }, [stocks]);
+
+  return (
+    <div div className="app">
+      <div className="ticker-search">
+        <form onSubmit={handleOnSubmit}>
+          <input
+            type="text"
+            value={ticker}
+            onChange={(e) => {
+              setTicker(e.target.value);
+            }}
+          />
+        </form>
+        {showStockInfo()}
+      </div>
+      <div className="assets">
+        <h2>Networth: {networth}</h2>
+        <h2>Yearly dividend: {passiveIncome}</h2>
+      </div>
+      <StockList
+        stocks={stocks}
+        updateQuantity={updateStockQuantity}
+        deleteStock={deleteStock}
+      />
+    </div>
+  );
+};
 export default App;
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,44 +1,61 @@
-import React from 'react';
+import React from "react";
 
+const StockList = ({ stocks, updateQuantity, deleteStock }) => {
+  const headers = [
+    "Stock",
+    "Price",
+    "quantity",
+    "Equaity",
+    "Dividend",
+    "Pasive Income",
+  ];
 
-const StockList = ({ stocks, updateQuantity }) => {
+  const mapHeaders = headers.map((item) => {
+    return <th style={{ width: 100 / 6 + "%" }}>{item}</th>;
+  });
 
-    const mappedStocks = stocks.map(item => {
-        return (
-            <tr key={item.symbol}>
-                <td className="stock-ticker">{item.symbol}</td>
-                <td className="stock-price">{item.price}</td>
-                <input
-                    type="number"
-                    onChange={(e) => { updateQuantity(e.target.value, item.symbol) }}
-                    value={item.quantity}
-                />
-                <td className="stock-equaity">{(item.price * item.quantity).toFixed(2)}</td>
-                <td className="stock-dividend">{item.dividend}%</td>
-                <td className="stock-passive-income">{(item.price * (item.dividend / 100)).toFixed(2) * item.quantity}</td>
-
-            </tr>
-        )
-    })
+  const mappedStocks = stocks.map((item) => {
     return (
-        <div className="stock-list">
-            <table style={{ width: '100%' }}>
-                <thead>
-                    <tr>
-                        <th style={{ width: (100 / 6) + '%' }}>Stock</th>
-                        <th style={{ width: (100 / 6) + '%' }}>Price</th>
-                        <th style={{ width: (100 / 6) + '%' }}>Quantity</th>
-                        <th style={{ width: (100 / 6) + '%' }}>Equaity</th>
-                        <th style={{ width: (100 / 6) + '%' }}>Dividend</th>
-                        <th style={{ width: (100 / 6) + '%' }}>Passive Income</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {mappedStocks}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+      <tr key={item.symbol}>
+        <td className="stock-ticker">{item.symbol}</td>
+        <td className="stock-price">{item.price}</td>
+        <input
+          type="number"
+          onChange={(e) => {
+            updateQuantity(e.target.value, item.symbol);
+          }}
+          value={item.quantity}
+        />
+        <td className="stock-equaity">
+          {(item.price * item.quantity).toFixed(2)}
+        </td>
+        <td className="stock-dividend">{item.dividend}%</td>
+        <td className="stock-passive-income">
+          {(item.price * (item.dividend / 100)).toFixed(2) * item.quantity}
+        </td>
+        <td>
+          <button
+            onClick={() => {
+              deleteStock(item.symbol);
+            }}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="stock-list">
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>{mapHeaders}</tr>
+        </thead>
+        <tbody>{mappedStocks}</tbody>
+      </table>
+    </div>
+  );
+};
 
 export default StockList;
