d5b5d02c2a72d6d3a76b57d820e496ab117e7a92
umpoy <iumpoy@yahoo.com>
2024-11-06 10:52:34 -0800

    feat: update scrapper to grab from yahoo

--- server/functions/getStockData.js
+++ server/functions/getStockData.js
@@ -2,29 +2,23 @@ const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
 module.exports = async function (symbol) {
-  const res = await fetch(
-    `https://www.google.com/finance/quote/${symbol}:NASDAQ?hl=en`
-  );
+  const res = await fetch(`https://finance.yahoo.com/quote/${symbol}/`);
   const text = await res.text();
   const $ = cheerio.load(text);
-  const findStockPrice = $(".YMlKec.fxKbKc")
-    .first()
+  const stockPrice = $(".livePrice span").text();
+  let dividend = 0;
+  const findDividend = $("span[title='Forward Dividend & Yield']")
+    .next()
     .text()
-    .replace(/[$,]/g, "")
-    .trim();
-  const findDividend = $(".gyFHrc")
-    .filter((i, el) => {
-      return $(el).find(".mfs7Fc").text().trim() === "Dividend yield";
-    })
-    .find(".P6K39c")
-    .text()
-    .replace(/[%]/g, "")
-    .trim();
+    .match(/\((\d+\.\d+)%\)/);
+  if (findDividend) {
+    dividend = findDividend[1];
+  }
 
   const data = {
     symbol: symbol,
-    price: Number(findStockPrice),
-    dividend: Number(findDividend),
+    price: Number(stockPrice),
+    dividend: Number(dividend),
   };
   return await data;
 };
